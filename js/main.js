3be17d6d309ce1502baf94c1640d9a58573c47eb
fatimasaliousarr-web <fatimasaliousarr@gmail.com>
2026-02-03 01:10:58 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
-const button = document.querySelector(".btn");
+const canvas = document.getElementById("particles");
+const ctx = canvas.getContext("2d");
 
-button.addEventListener("click", function() {
-  document.querySelector("#projects").scrollIntoView({
-    behavior: "smooth"
-  });
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let stars=[];
+
+for(let i=0;i<120;i++){
+stars.push({
+x:Math.random()*canvas.width,
+y:Math.random()*canvas.height,
+r:Math.random()*2
+});
+}
+
+function animate(){
+ctx.clearRect(0,0,canvas.width,canvas.height);
+
+stars.forEach(s=>{
+ctx.fillStyle="white";
+ctx.beginPath();
+ctx.arc(s.x,s.y,s.r,0,Math.PI*2);
+ctx.fill();
+s.y+=0.3;
+if(s.y>canvas.height) s.y=0;
 });
 
+requestAnimationFrame(animate);
+}
+
+animate();
